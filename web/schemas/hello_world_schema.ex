778a2074da85e81ed3a9bb23e7b9fee0b684eebe
Carlos Navas <carlos.navas@admanmedia.com>
2019-07-18 14:05:09 +0200

    adding interface for schema

--- web/schemas/hello_world_schema.ex
+++ web/schemas/hello_world_schema.ex
@@ -2,6 +2,10 @@ defmodule ElixirPlug.Web.Schemas.HelloWorld do
   @moduledoc """
   Schema for /api/hello_world
   """
+  alias ElixirPlug.Web.Controllers.ISchema
+
+  @behaviour ISchema
+
   @schema %{
     "name" => %Litmus.Type.String{
       min_length: 6,
@@ -10,6 +14,7 @@ defmodule ElixirPlug.Web.Schemas.HelloWorld do
     }
   }
 
+  @impl ISchema
   def get_schema, do: @schema
 
 end
